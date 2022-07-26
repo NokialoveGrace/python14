96af536f376e103df9dcb9f284d2d00fd89242af
NokialoveGrace <Nokia5212310>
2022-07-26 14:58:41 +0700

    function

---
+++ HM function.py
@@ -0,0 +1,13 @@
+from cmath import pi
+
+
+def square(width,height):
+   sum =width*height
+def triangle(base,height):
+    sum =(base*height)/2
+    return sum
+def circle(radius):
+    sum = (radius*radius)*pi
+    return sum
+
+#นายอรรถสิทธิ์ ไทรทอง ม.6/14 เลขที่12
