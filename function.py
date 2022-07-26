f1292dbe3b66432a2d970c01205df1e853cc3008
NokialoveGrace <Nokia5212310>
2022-07-26 14:08:35 +0700

    function

---
+++ function.py
@@ -0,0 +1,12 @@
+#
+def hello():
+    print("Hello world")
+    print("Auttasit")
+#เรียกใช้งานฟังก์ชัน
+#ส่งค่าผ่านparameter
+#ส่งค่าผ่านparametr+return
+def sum(x,y):
+    return x+y
+ans = sum(8,6)/2
+print(ans)
+ 
